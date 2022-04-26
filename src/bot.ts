9fd2042c0db74c02d17fbf772293c75702c0a508
kapturoff <pigunderwater@gmail.com>
2022-04-26 19:30:01 +0300

    ci: edit server for running multiple bot instances

--- src/bot.ts
+++ src/bot.ts
@@ -1,4 +1,4 @@
-import { Bot, session } from 'grammy'
+import { Bot, session, Api, RawApi } from 'grammy'
 import { I18n } from '@grammyjs/i18n'
 import * as path from 'path'
 import { BotContext } from './interfaces'
@@ -6,57 +6,54 @@ import confirmLanguageMiddleware, {
     switchLang,
 } from './helpers/confirmLanguageMiddleware'
 
-// If there's no bot token in environment variables
-if (!process.env.BOT_TOKEN) {
-    console.error('BOT_TOKEN was not provided.')
-    process.exit(1)
-}
+export default function buildBot(
+    botToken: string
+): Bot<BotContext, Api<RawApi>> {
+    // Create instance of a bot uses our BotContext
+    const bot = new Bot<BotContext>(botToken)
 
-// Create instance of a bot uses our BotContext
-const bot = new Bot<BotContext>(process.env.BOT_TOKEN)
+    // Registering middlewares
 
-// Registering middlewares
+    // Allow bot to use sessions
+    bot.use(
+        session({
+            initial: () => ({ lang: '' }),
+        })
+    )
 
-// Allow bot to use sessions
-bot.use(
-    session({
-        initial: () => ({ lang: '' }),
+    // Configure localization
+    const i18n = new I18n({
+        defaultLanguageOnMissing: true, // If something missing in chosen lang, take it from main language
+        directory: path.resolve(__dirname, 'locales'), // Directory where locales stored
+        useSession: true, // Allow to use session to remember users' langs
+        defaultLanguage: 'en',
     })
-)
 
-// Configure localization
-const i18n = new I18n({
-    defaultLanguageOnMissing: true, // If something missing in chosen lang, take it from main language
-    directory: path.resolve(__dirname, 'locales'), // Directory where locales stored
-    useSession: true, // Allow to use session to remember users' langs
-    defaultLanguage: 'en',
-})
+    /**
+     * After adding it as a middleware, localization becomes available
+     * through ctx.i18n. ctx.i18n.t() can pick a value from locales and
+     * template it with given to this function data.
+     */
+    bot.use(i18n.middleware())
 
-/**
- * After adding it as a middleware, localization becomes available
- * through ctx.i18n. ctx.i18n.t() can pick a value from locales and
- * template it with given to this function data.
- */
-bot.use(i18n.middleware())
+    // Add a middleware that asks user to choose his language everytime it's not set
+    bot.use(confirmLanguageMiddleware)
 
-// Add a middleware that asks user to choose his language everytime it's not set
-bot.use(confirmLanguageMiddleware)
+    // Commands
 
-// Commands
+    bot.command('start', async (ctx) => {
+        await ctx.reply(ctx.i18n.t('chat.welcome', { cityName: 'Krasnodar' }))
+    })
 
-bot.command('start', async (ctx) => {
-    await ctx.reply(ctx.i18n.t('chat.welcome', { cityName: 'Krasnodar' }))
-})
+    // Switching to Russian
+    bot.command('rulang', async (ctx) => {
+        await switchLang(ctx, 'ru')
+    })
 
-// Switching to Russian
-bot.command('rulang', async (ctx) => {
-    await switchLang(ctx, 'ru')
-})
+    // Switching to English
+    bot.command('enlang', async (ctx) => {
+        await switchLang(ctx, 'en')
+    })
 
-// Switching to English
-bot.command('enlang', async (ctx) => {
-    await switchLang(ctx, 'en')
-})
-
-// Export bot to run it in index.ts
-export default bot
+    return bot
+}
--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,100 @@
+// 1. Add an array into server where the bot instances going to be stored
+// 2. Endpoint template /bot<endpoint> for handling bot requests
+// 3. Add and delete bot from an array
+
 // Loading .env file and put its values to process.env
 import { config } from 'dotenv'
 config()
 
-// Pick all variables from process.env
-const { BOT_TOKEN, LOG_API, WEBHOOK_URL } = process.env
+// Pick all needed variables from process.env
+const { LOG_API, WEBHOOK_URL } = process.env
 
 // Importing all neccessary code
 import Fastify from 'fastify'
-import { webhookCallback } from 'grammy'
-import bot from './bot'
+import { webhookCallback, Bot } from 'grammy'
+import buildBot from './bot'
+import * as i from './interfaces'
+import { Low, JSONFile } from 'lowdb'
 
 // Creating the server for obtaining webhooks
 const app = Fastify({
     logger: Boolean(LOG_API),
 })
 
-/**
- * Adding handler for endpoint where the webhooks are going to be sent.
- * By that, incoming requests from api.telegram.org will be served by bot as updates.
- *
- * It's important in production to set hash for this endpoint name instead of bot token,
- * because it can be leaked from API logs.
- *
- * @see https://grammy.dev/guide/deployment-types.html#how-to-use-webhooks
- */
-app.post('/' + BOT_TOKEN, webhookCallback(bot, 'fastify'))
+const botsInitiliazed: i.BotMixed[] = []
 
-app.listen(process.env.PORT, (err, address) => {
+/**
+ * Because we have multiple bots running, everytime that request comes to an endpoint,
+ * we need to find a responsible bot that is responsible for this endpoint and delegate
+ * a work for handling the request to it.
+ */
+app.post<{
+    Params: i.Params
+}>('/:endpoint', (req, reply) => {
+    if (!req.params?.endpoint) throw new Error('Endpoint must be set.')
+
+    // Looking for a bot that is responsible for handling requests on that endpoint
+    const responsibleBot = botsInitiliazed.find(
+        ({ botData }) => botData.endpoint == req.params.endpoint
+    )
+
+    if (!responsibleBot)
+        throw new Error(
+            'There is no bot that is responsible for this endpoint.'
+        )
+
+    // Allowing bot to handle incoming request
+    webhookCallback(responsibleBot.botInstance, 'fastify')(req, reply)
+})
+
+// Starting the server
+
+app.listen(process.env.PORT, async (err, address) => {
     if (err) throw err // TODO: Create error handler for bot things
 
     /**
-     * Notifying Telegram API to send updates to WEBHOOK_URL + BOT_TOKEN.
+     * Initializing database.
      *
-     * Example: http://example.com/ABCDefghIjK1234567890
+     * **Important**: Database need to be filled by the time you run the server.
+     *
+     * You could fill it by yourself according interface DatabaseSchema or by using
+     * the script fillDB.js (*npm run fill_db*) that is in the root of repository.
      */
-    bot.api.setWebhook(WEBHOOK_URL + BOT_TOKEN).catch(console.error)
+    const adapter = new JSONFile<i.DatabaseSchema>('db.json'),
+        db = new Low(adapter)
+
+    // Reading the database
+    await db.read()
+
+    // Getting data about all the bots that need to be running right after server initialized
+    const { bots } = db.data
+
+    for (const botData of bots) {
+        const bot = buildBot(botData.botToken) // Initilize new bot
+
+        /**
+         * Add a just initilized bot to an array of running bots, so it
+         * will be handling incoming from Telegram API requests as any other
+         * running bot.
+         *
+         * This approach is chosen because of its flexibility in meaning of that
+         * will be quite easy to stop the bot for a while or add a new one in the
+         * future.
+         */
+        botsInitiliazed.push({
+            botInstance: bot,
+            botData,
+        })
+
+        /**
+         * Notifying Telegram API to send updates to WEBHOOK_URL + BOT_TOKEN.
+         *
+         * Example: http://example.com/ABCDefghIjK1234567890
+         */
+        await bot.api
+            .setWebhook(WEBHOOK_URL + botData.endpoint)
+            .catch(console.error)
+    }
 
     console.info(`[info]: server's now listening in ${address}`)
 })
